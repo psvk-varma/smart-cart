5929280065d765a1afe0871ca037633134435e54
Vijay Potturi <vijaypotturi2006@gmail.com>
2026-03-02 17:00:52 +0530

    Add default values for env variables

--- config.py
+++ config.py
@@ -4,12 +4,14 @@ from dotenv import load_dotenv
 load_dotenv()
 
 class Config:
-    SECRET_KEY = os.getenv("SECRET_KEY")
+    SECRET_KEY = os.getenv("SECRET_KEY", "dev-secret-key")
+
     SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = 'smtp.gmail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
-    MAIL_USERNAME = os.getenv("MAIL_USERNAME")
-    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD")
+
+    MAIL_USERNAME = os.getenv("MAIL_USERNAME", "")
+    MAIL_PASSWORD = os.getenv("MAIL_PASSWORD", "")
