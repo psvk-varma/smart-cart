2ea775586eef42ab36cd3b2576301e2efb9b5788
Vijay Potturi <vijaypotturi2006@gmail.com>
2026-03-02 16:48:51 +0530

    Fix database path for Vercel

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_DATABASE_URI = "sqlite:////tmp/database.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     MAIL_SERVER = 'smtp.gmail.com'
